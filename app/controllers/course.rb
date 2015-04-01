949f460a7c6ca2fd9e0553b66f78205c021480ea
Stephanie Lo <lo.stephaniec@gmail.com>
2015-04-01 11:14:23 -0400

    add capability to add new courses

--- app/controllers/course.rb
+++ app/controllers/course.rb
@@ -2,5 +2,20 @@ get '/user/:id/course/:course' do
   user = User.find_by(id: params[:id])
   course = Course.find_by(id: params[:course])
   scores = user.scores.where(course_id: course.id)
-  erb :'score/scores', locals:{user: user, course: course, scores: scores}
+  erb :'course/details', locals:{user: user, course: course, scores: scores}
+end
+
+get '/course/new' do
+  erb :'course/new'
+end
+
+post '/course/new' do
+  course = Course.new(  name: params[:name],
+                        course: params[:course],
+                        slope: params[:slope])
+  if course.save!
+    redirect '/course/details'
+  else
+    [500, "something went wrong in the creation!"]
+  end
 end
