b085d4d15bcc972dea808ac01fe95cf8f4cee7ac
Stephanie Lo <lo.stephaniec@gmail.com>
2015-04-01 09:50:57 -0400

    got auth routes

---
+++ app/controllers/auth.rb
@@ -0,0 +1,35 @@
+get '/auth/login' do
+  erb :'auth/login'
+end
+
+put '/auth/login' do
+  user = User.find_by(username: params[:username])
+
+  if user.try(:authenticate, params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    session[:message] = "Incorrect username and/or password!"
+    redirect '/auth/login'
+  end
+end
+
+get '/auth/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+get '/auth/signup' do
+  erb :'auth/signup'
+end
+
+post '/auth/signup' do
+  user = User.new(  username: params[:username],
+                    password: params[:password])
+  if user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    [500, "Uh oh! Made a mistake!"]
+  end
+end
