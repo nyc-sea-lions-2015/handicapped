73f1bbd37bb5c573f4081631dcf86e135368ad57
Stephanie Lo <lo.stephaniec@gmail.com>
2015-04-01 11:23:21 -0400

    got course details up

--- app/controllers/course.rb
+++ app/controllers/course.rb
@@ -2,7 +2,7 @@ get '/user/:id/course/:course' do
   user = User.find_by(id: params[:id])
   course = Course.find_by(id: params[:course])
   scores = user.scores.where(course_id: course.id)
-  erb :'course/details', locals:{user: user, course: course, scores: scores}
+  erb :'course/stats', locals:{user: user, course: course, scores: scores}
 end
 
 get '/course/new' do
@@ -14,8 +14,13 @@ post '/course/new' do
                         course: params[:course],
                         slope: params[:slope])
   if course.save!
-    redirect '/course/details'
+    redirect "/course/#{course.id}/details"
   else
     [500, "something went wrong in the creation!"]
   end
+end
+
+get '/course/:id/details' do
+  course = Course.find_by(id: params[:id])
+  erb :'course/details', locals:{course: course}
 end
